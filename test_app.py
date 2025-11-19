63eab82408f46aafa305994d11bf9c50f1006221
tunahanyilmaz25 <tunahanyilmazz@gmail.com>
2025-11-19 16:09:08 +0300

    default region added

--- test_app.py
+++ test_app.py
@@ -5,8 +5,9 @@ This can be expanded with proper unit tests using pytest.
 import os
 import sys
 
-# Set environment variable before importing app
+# Set environment variables before importing app
 os.environ['DYNAMODB_TABLE_NAME'] = 'picus_data'
+os.environ['AWS_DEFAULT_REGION'] = os.environ.get('AWS_DEFAULT_REGION', 'eu-central-1')
 
 def test_app_import():
     """Test that the app can be imported successfully."""
